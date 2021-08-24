3f14e14e2349f1721a674dec9b72a5f769d2a031
milicabjekovic <milicabjekovic@gmail.com>
2021-08-24 08:13:38 +0200

    examination test, feedback test

--- PSV/UnitTests/FeedbackTest.cs
+++ PSV/UnitTests/FeedbackTest.cs
@@ -13,64 +13,6 @@ namespace UnitTests
     public class FeedbackTest
     {
 
-        [TestMethod]
-        public void WriteUserFeedback()
-        {
-            FeedbackController controller = new FeedbackController(new PSV.Configuration.ProjectConfiguration());
-
-            Feedback feedback = new Feedback();
-            feedback.Deleted = false;
-            feedback.Feed = "Test";
-            feedback.IsPublish = false;
-            feedback.PatientEmail = "test@gmail.com";
-
-            var result = controller.Add(feedback);
-
-
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void ReadAdminFeedback()
-        {
-            FeedbackController controller = new FeedbackController(new PSV.Configuration.ProjectConfiguration());
-
-            
-
-            var result = controller.GetAllAdminFeedbacks();
-
-
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void ReadPatientFeedback()
-        {
-            FeedbackController controller = new FeedbackController(new PSV.Configuration.ProjectConfiguration());
-
-
-
-            var result = controller.getAllPatinetFeedbacks();
-
-
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void ShareUserFeedbackByAdmin()
-        {
-            FeedbackController controller = new FeedbackController(new PSV.Configuration.ProjectConfiguration());
-
-            Feedback feedback = new Feedback();
-            feedback.Deleted = false;
-            feedback.Feed = "Test";
-            feedback.IsPublish = true;
-            feedback.PatientEmail = "test@gmail.com";
-
-            var result = controller.AddPublishFeedback(feedback.Id);
-
-
-            Assert.IsNotNull(result);
-        }
+        
     }
 }
